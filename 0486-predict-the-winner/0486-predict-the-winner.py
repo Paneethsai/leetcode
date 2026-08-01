a03a47216aae4b29ebf4e445aa8133963483e9e7
PANEETH SAI <ai.paneeth@gmail.com>
2026-08-01 21:49:45 +0530

    Time: 0 ms (100%), Space: 12.3 MB (93.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def predictTheWinner(self, nums):
+        n=len(nums)
+        if n%2==0:
+            return True
+        dp=list(nums)
+        for i in range(n-2,-1,-1):
+            for j in range(i+1,n):
+                dp[j]=max(nums[i]-dp[j],nums[j]-dp[j-1])
+        return dp[-1]>=0
